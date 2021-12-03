cdee486c3cd82f73e0c4eb033f4d0004d704c52f
Holger Arndt <holger.arndt@hetzner.com>
2021-12-03 08:22:53 +0100

    2021: Day 3 optimizations

--- 2021/day_3.cr
+++ 2021/day_3.cr
@@ -30,46 +30,21 @@ def part1(input)
   gamma.to_i32(2) * epsilon.to_i32(2)
 end
 
+def scan_for(subset, msb = '1', lsb = '0')
+  subset.first.size.times do |pos|
+    occurance = subset.map { |ele| ele[pos] }.tally
+    subset = subset.select do |ele|
+      ele[pos] == (occurance.fetch('0', 0) <= occurance.fetch('1', 0) ? msb : lsb)
+    end
+
+    return subset.first if subset.size == 1
+  end
+end
+
 def part2(input)
-  o2 = ""
-  co2 = ""
-
   list = input.split("\n")
-  subset = list
-  list.first.size.times do |pos|
-    occurance = subset.map { |ele| ele[pos] }.tally
-    if occurance.fetch('0', 0) <= occurance.fetch('1', 0)
-      o2 += "1"
-    else
-      o2 += "0"
-    end
-    subset = subset.select do |ele|
-      ele[pos] == o2[pos]
-    end
-
-    if subset.size == 1
-      o2 = subset.first
-      break
-    end
-  end
-
-  subset = list
-  list.first.size.times do |pos|
-    occurance = subset.map { |ele| ele[pos] }.tally
-    if occurance.fetch('0', 0) > occurance.fetch('1', 0)
-      co2 += "1"
-    else
-      co2 += "0"
-    end
-    subset = subset.select do |ele|
-      ele[pos] == co2[pos]
-    end
-
-    if subset.size == 1
-      co2 = subset.first
-      break
-    end
-  end
+  o2 = scan_for(list, '1', '0') || '0'
+  co2 = scan_for(list, '0', '1') || '0'
 
   o2.to_i32(2) * co2.to_i32(2)
 end
